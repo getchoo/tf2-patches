102f1fba09c28a90ce754e0f21b2f8bc0c2882e0
ReplayCoding <replaycoding@gmail.com>
2023-03-07 12:17:52 -0800

    don't automatically use mangohud
    users can enabled mangohud (or other tools) by setting the LAUNCH_PREFIX
    environment variable.

--- game_clean/copy/start_tf2.sh
+++ game_clean/copy/start_tf2.sh
@@ -4,7 +4,4 @@ cd "$(dirname "$0")"
 
 ARGS="-steam -game tf -insecure -novid -nojoy -nosteamcontroller -particle_fallback 2 -nobreakpad"
 
-export LAUNCH_PREFIX="$HOME/.steam/bin32/steam-runtime/run.sh"
-LAUNCH_PREFIX+=" $(command -v mangohud || true)"
-
-${LAUNCH_PREFIX} "$(pwd)"/hl2.sh ${ARGS} $@
+$HOME/.steam/bin32/steam-runtime/run.sh ${LAUNCH_PREFIX} "$(pwd)"/hl2.sh ${ARGS} $@
