01b4af7829a243e16bf24df98527102ed23aaf44
ReplayCoding <replaycoding@gmail.com>
2023-03-18 17:55:07 -0400

    mirror script change from windows

--- game_clean/copy/start_tf2.sh
+++ game_clean/copy/start_tf2.sh
@@ -2,6 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 
-ARGS="-steam -game tf -insecure -novid -nojoy -nosteamcontroller -particle_fallback 2 -nobreakpad"
+ARGS="-steam -game tf -insecure -novid -nojoy -nosteamcontroller -nobreakpad"
 
 $HOME/.steam/bin32/steam-runtime/run.sh ${LAUNCH_PREFIX} "$(pwd)"/hl2.sh ${ARGS} $@
